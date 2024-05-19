7e80d73e3c05e6b112facd57217d78b7f1d391e1
Donal-Noye <leekeiz49@gmail.com>
2024-05-20 00:50:10 +0300

    added .html in public

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			config.resolve.fallback = {
-				fs: false,
-				module: false,
-				path: false,
-			};
-		}
-		return config;
-	},
-	headers: async () => [
-		{
-			source: '/:path*',
-			headers: [
-				{
-					key: 'Content-Security-Policy',
-					value: "meta-src 'self' data:;",
-				},
-				{
-					key: 'meta',
-					value: 'cryptomus c7cbe90c',
-				},
-			],
-		},
-	],
+
 };
 
 export default nextConfig;
